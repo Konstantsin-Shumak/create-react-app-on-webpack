ce6fa4eec11ade3872c16aa498d6cb282b7b6e45
Konstantsin Shumak <konstantsin.shumak@gmail.com>
2022-10-02 04:23:32 +0300

    feat: add slider library

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,68 @@
 import React from "react";
+import WebkitInputRangeFillLower from "../scripts/WebkitInputRangeFillLower";
 
 export const App = () => {
+  React.useEffect(() => {
+    new WebkitInputRangeFillLower({
+      selectors: ["rus-range", "mounth-range", "percent-range"],
+      angle: 90,
+      color: "#ff9514",
+    });
+  }, []);
   return (
-    <>
-      <h1>React Init</h1>
-      <p>Testing</p>
-    </>
+    <div className="wrapper">
+      <h1 className="wrapper__title">
+        Рассчитайте стоимость
+        <br />
+        автомобиля в лизинг
+      </h1>
+      <div className="wrapper__input-blocks">
+        <div className="input-block">
+          <p className="input-block__title">Стоимость автомобиля</p>
+          <div className="input-block__input-content">
+            <div className="input-content__input">
+              <input type="number" />
+              <span>₽</span>
+            </div>
+            <input type="range" min="0" max="100000" id="rus-range" />
+          </div>
+        </div>
+
+        <div className="input-block">
+          <p className="input-block__title">Первоначальный взнос</p>
+          <div className="input-block__input-content">
+            <div className="input-content__input">
+              <input type="number" />
+              <span>%</span>
+            </div>
+            <input type="range" min="0" max="100000" id="percent-range" />
+          </div>
+        </div>
+
+        <div className="input-block">
+          <p className="input-block__title">Срок лизинга</p>
+          <div className="input-block__input-content">
+            <div className="input-content__input">
+              <input type="number" />
+              <span>мес.</span>
+            </div>
+            <input type="range" min="0" max="100000" id="mounth-range" />
+          </div>
+        </div>
+      </div>
+      <div className="wrapper__output-blocks">
+        <div className="output-block">
+          <p>Сумма договора лизинга</p>
+          <h2>4 467 313 ₽</h2>
+        </div>
+
+        <div className="output-block">
+          <p>Ежемесячный платех от</p>
+          <h2>114 455 ₽</h2>
+        </div>
+
+        <button>Оставить заявку</button>
+      </div>
+    </div>
   );
 };
