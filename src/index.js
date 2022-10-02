0ec275a17ff898af361c9c1ef35171d33b5e08de
Konstantsin Shumak <konstantsin.shumak@gmail.com>
2022-10-03 01:54:19 +0300

    feat: add test deploy

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App.jsx';
-import "./assets/style/global.css";
-
-
-createRoot(document.getElementById('root')).render(<App />);
