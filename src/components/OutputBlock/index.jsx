b27216cc897096043e7cdc9ca2adfdf5d145190c
Konstantsin Shumak <konstantsin.shumak@gmail.com>
2022-10-03 19:52:24 +0300

    refactor: rename links

--- src/components/OutputBlock/index.jsx
+++ src/components/OutputBlock/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { formatingNumner } from "../../scripts/formatingNumber";
+import { formatingNumber } from "../../scripts/formatingNumber";
 
 export const OutputBlock = ({ title, value = 0, currency }) => {
   return (
     <div className="output-block">
       <p className="output-block__title">{title}</p>
       <div className="output-block__result">
-        <h2>{formatingNumner(value)}</h2>
+        <h2>{formatingNumber(value)}</h2>
         <span className="result__currency">{currency}</span>
       </div>
     </div>
